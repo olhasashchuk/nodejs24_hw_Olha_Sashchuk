04bd2d1073666cda4ba710d088f8b8c7fcb7844d
Olha Sashchuk <olha.sashchuk@gmail.com>
2024-10-21 14:25:16 +0200

    implemented controller ip

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LogIpMiddleware } from './logIp/middleware';
+import { RequestController } from './request/request.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, RequestController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
